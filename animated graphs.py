7eb3e619e7f49dae0296dde8debe96e37537d0df
kavash11 <80080693+kavash11@users.noreply.github.com>
2023-03-25 17:30:02 -0400

    live graphs

--- animated graphs.py
+++ animated graphs.py
@@ -1,7 +1,4 @@
-import tkinter as tk
-from tkinter import filedialog, Text
-from tkinter import *
-import os
+
 import serial
 import numpy as np
 import csv
@@ -10,10 +7,11 @@ import matplotlib.pyplot as pp
 import matplotlib.animation as animation
 import datetime
 
+ser = serial.Serial('COM6',115200)
 
 
 def getKnee(i):
-    ser = serial.Serial('COM6',115200)
+    
     ser.reset_output_buffer()
     ser.reset_input_buffer()
 
@@ -42,7 +40,7 @@ def getKnee(i):
 
 
 def getHip(i1):
-    ser = serial.Serial('COM6',115200)
+    #ser = serial.Serial('COM6',115200)
     ser.reset_output_buffer()
     ser.reset_input_buffer()
 
@@ -61,19 +59,19 @@ def getHip(i1):
     count.append(i1)
     print (count, hip)
     
-    line1.set_data(count, hip)
+    line2.set_data(count, hip)
 
     #line3.set_data(count, toe)
     #line4.set_data(count, heel)
     
     print("got here3")
-    ax1.relim()
-    ax1.autoscale_view()
+    ax2.relim()
+    ax2.autoscale_view()
     i1 +=1
 
 
 def getToe(i2):
-    ser = serial.Serial('COM6',115200)
+    #ser = serial.Serial('COM6',115200)
     ser.reset_output_buffer()
     ser.reset_input_buffer()
 
@@ -93,19 +91,19 @@ def getToe(i2):
     count.append(i2)
     print (count, toe)
     
-    line1.set_data(count, toe)
+    line3.set_data(count, toe)
 
     #line3.set_data(count, toe)
     #line4.set_data(count, heel)
     
     print("got here3")
-    ax1.relim()
-    ax1.autoscale_view()
+    ax3.relim()
+    ax3.autoscale_view()
     i2 +=1
 
 
 def getHeel(i3):
-    ser = serial.Serial('COM6',115200)
+    #ser = serial.Serial('COM6',115200)
     ser.reset_output_buffer()
     ser.reset_input_buffer()
 
@@ -125,41 +123,53 @@ def getHeel(i3):
     count.append(i3)
     print (count, heel)
     
-    line1.set_data(count, heel)
+    line4.set_data(count, heel)
 
     #line3.set_data(count, toe)
     #line4.set_data(count, heel)
     
     print("got here3")
-    ax1.relim()
-    ax1.autoscale_view()
+    ax4.relim()
+    ax4.autoscale_view()
     i3 +=1
 
 
-def graph(func):  
-    plotanimation1 = animation.FuncAnimation(fig, func, interval=100)
-    pp.show()
-
-
+##def graph(func):  
+##    plotanimation1 = animation.FuncAnimation(fig, func, interval=100)
+##    pp.show()
+##
+##
 knee = []
 hip = []
 toe = []
 heel = []
-fig, ax1=pp.subplots()
-line1 = pp.plot((0),(0),c='black')[0]
 i = 0
 i1=0
 i2=0
 i3=0
 count = []
+fig, ((ax1, ax2), (ax3, ax4)) =pp.subplots(2,2)
+line1 = ax1.plot((0),(0),c='black')[0]
+#fig, ax2=pp.subplots()
+line2 = ax2.plot((0),(0),c='black')[0]
+#fig, ax3=pp.subplots()
+line3 = ax3.plot((0),(0),c='black')[0]
+#fig, ax4=pp.subplots()
+line4 = ax4.plot((0),(0),c='black')[0]
 
-def main():
-    root = tk.Tk()
-    root.geometry("400x200")
 
-    button3 = Button(root, text='Toe Pressure', command=graph(getToe)) #buttons raen't working, graphs both sensors on same line on same plot
-    button3.pack()
-    button4 = Button(root, text='Heel Pressure', command=graph(getHeel))
-    button4.pack()
-    root.mainloop()
-main()
+plotanimation1 = animation.FuncAnimation(fig, getKnee, interval=100)
+plotanimation2 = animation.FuncAnimation(fig, getHip, interval=100)
+plotanimation3 = animation.FuncAnimation(fig, getToe, interval=100)
+plotanimation4 = animation.FuncAnimation(fig, getHeel, interval=100)
+pp.show()
+##def main():
+##    root = tk.Tk()
+##    root.geometry("400x200")
+##
+##    button3 = Button(root, text='Toe Pressure', command=graph(getToe)) #buttons raen't working, graphs both sensors on same line on same plot
+##    button3.pack()
+##    button4 = Button(root, text='Heel Pressure', command=graph(getHeel))
+##    button4.pack()
+##    
+##main()
