b15169014d8a71581976a1649f4f1df06fe42ff4
kavash11 <80080693+kavash11@users.noreply.github.com>
2023-03-25 11:06:50 -0400

    animated graphs

---
+++ animated graphs.py
@@ -0,0 +1,168 @@
+import tkinter as tk
+from tkinter import filedialog, Text
+from tkinter import *
+import os
+import serial
+import numpy as np
+import csv
+import time
+import matplotlib.pyplot as pp
+import matplotlib.animation as animation
+import datetime
+
+
+
+def getKnee(i):
+    ser = serial.Serial('COM6',115200)
+    ser.reset_output_buffer()
+    ser.reset_input_buffer()
+
+    t0 = time.time()
+
+    data = ser.readline()
+
+    data = data.strip()
+    data = str(data)
+    data= data[2:-1]
+    data = list(map(str.strip, data.split(',')))
+    knee.append(float(data[0]))
+    
+    count.append(i)
+    print (count, knee)
+    
+    line1.set_data(count, knee)
+
+    #line3.set_data(count, toe)
+    #line4.set_data(count, heel)
+    
+    print("got here3")
+    ax1.relim()
+    ax1.autoscale_view()
+    i +=1
+
+
+def getHip(i1):
+    ser = serial.Serial('COM6',115200)
+    ser.reset_output_buffer()
+    ser.reset_input_buffer()
+
+    t0 = time.time()
+
+    data = ser.readline()
+
+    data = data.strip()
+    data = str(data)
+    data= data[2:-1]
+    data = list(map(str.strip, data.split(',')))
+
+    hip.append(float(data[1]))
+
+    
+    count.append(i1)
+    print (count, hip)
+    
+    line1.set_data(count, hip)
+
+    #line3.set_data(count, toe)
+    #line4.set_data(count, heel)
+    
+    print("got here3")
+    ax1.relim()
+    ax1.autoscale_view()
+    i1 +=1
+
+
+def getToe(i2):
+    ser = serial.Serial('COM6',115200)
+    ser.reset_output_buffer()
+    ser.reset_input_buffer()
+
+    t0 = time.time()
+
+    data = ser.readline()
+
+    data = data.strip()
+    data = str(data)
+    data= data[2:-1]
+    data = list(map(str.strip, data.split(',')))
+    knee.append(float(data[0]))
+    hip.append(float(data[1]))
+    toe.append(float(data[2]))
+    heel.append(float(data[3]))
+    
+    count.append(i2)
+    print (count, toe)
+    
+    line1.set_data(count, toe)
+
+    #line3.set_data(count, toe)
+    #line4.set_data(count, heel)
+    
+    print("got here3")
+    ax1.relim()
+    ax1.autoscale_view()
+    i2 +=1
+
+
+def getHeel(i3):
+    ser = serial.Serial('COM6',115200)
+    ser.reset_output_buffer()
+    ser.reset_input_buffer()
+
+    t0 = time.time()
+
+    data = ser.readline()
+
+    data = data.strip()
+    data = str(data)
+    data= data[2:-1]
+    data = list(map(str.strip, data.split(',')))
+    knee.append(float(data[0]))
+    hip.append(float(data[1]))
+    toe.append(float(data[2]))
+    heel.append(float(data[3]))
+    
+    count.append(i3)
+    print (count, heel)
+    
+    line1.set_data(count, heel)
+
+    #line3.set_data(count, toe)
+    #line4.set_data(count, heel)
+    
+    print("got here3")
+    ax1.relim()
+    ax1.autoscale_view()
+    i3 +=1
+
+
+def graph(func):  
+    plotanimation1 = animation.FuncAnimation(fig, func, interval=100)
+    pp.show()
+
+
+knee = []
+hip = []
+toe = []
+heel = []
+fig, ax1=pp.subplots()
+line1 = pp.plot((0),(0),c='black')[0]
+i = 0
+i1=0
+i2=0
+i3=0
+count = []
+
+def main():
+    root = tk.Tk()
+    root.geometry("400x200")
+    button1 = Button(root, text='Knee Flexion', command=graph(getKnee))
+    button1.pack()
+    button2 = Button(root, text='Hip Flexion', command=graph(getHip))
+    button2.pack()
+    button3 = Button(root, text='Toe Pressure', command=graph(getToe))
+    button3.pack()
+    button4 = Button(root, text='Heel Pressure', command=graph(getHeel))
+    button4.pack()
+    root.mainloop()
+main()
--- serial comm.py
+++ serial comm.py
@@ -3,12 +3,11 @@ import numpy as np
 import csv
 import time
 import matplotlib.pyplot as pp
+import matplotlib.animation as animation
 import datetime
 
-def main():
-    serial_port='COM6'
-    baud_rate = 115200
-    ser = serial.Serial(serial_port, baud_rate)
+def getData(hip, knee, heel, toe, ser):
+    
 
     #time_formatted = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
 
@@ -17,34 +16,40 @@ def main():
     ser.reset_output_buffer()
     ser.reset_input_buffer()
     
+    #knee=[]
+
+
+    t0 = time.time()
+
+
+    data = ser.readline()
+        #data = data.decode("utf-8")
+        
+    data = data.strip()
+    data = str(data)
+    data= data[2:-1]
+    data = list(map(str.strip, data.split(',')))
+    knee.append(float(data[0]))
+    hip.append(float(data[1]))
+    toe.append(float(data[2]))
+    heel.append(float(data[3]))
+    return(knee, hip, toe, heel)
+    
+    
+
+
+def animate(i):
     knee=[]
     hip=[]
     toe=[]
     heel=[]
-
-    t0 = time.time()
-
-    while True:
-        data = ser.readline()
-        #data = data.decode("utf-8")
-        
-        data = data.strip()
-        data = str(data)
-        data= data[2:-1]
-        data = list(map(str.strip, data.split(',')))
-        knee.append(float(data[0]))
-        hip.append(float(data[1]))
-        toe.append(float(data[2]))
-        heel.append(float(data[3]))
-        #data=data.split(",")
-        #print(data.split(","))
-        #knee.append(data[0])
-        #pp.plot(len(knee),knee, label = 'Knee Flexion')
-        #pp.xlabel('Time')
-        #pp.ylabel('Angle')
-        #pp.title('Knee Flexion')
-        #pp.show()
-        print(heel)
-    ser.close()
-
-main()
+    fig = pp.figure()
+    ax1=fig.add_subplot(1,1,1)
+    ser = serial.Serial('COM6',115200)
+    while (1):
+        knee, hip, toe, heel = getData(knee, hip, toe, heel, ser)
+        ax1.plot(knee, hip)
+        ani = animation.FuncAnimation(fig, animate, interval=100)
+        pp.show()
+#ser.close()
+animate(i=0)
